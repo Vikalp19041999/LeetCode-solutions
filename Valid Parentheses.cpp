0f9ef21bd2e33b15a02f51077da057544e62d838
Vikalp Chakravorty <vicks548@gmail.com>
2021-06-02 08:45:45 +0530

    LeetCode solutions

---
+++ Valid Parentheses.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        int length = s.length();
+        stack <char> st;
+        for(int i=0; i<length; i++) {
+            if(s[i]=='(' || s[i]=='{' || s[i]=='[') {
+                st.push(s[i]);
+            } else {
+                if(st.empty()) {
+                    return false;
+                } else if(s[i]==')' && st.top()!='(') {
+                    return false;
+                } else if(s[i]==']' && st.top()!='[') {
+                    return false;
+                } else if(s[i]=='}' && st.top()!='{') {
+                    return false;
+                } else {
+                    st.pop();
+                }
+            }
+        }
+        if(st.empty()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+};
