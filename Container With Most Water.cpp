f8419627e3151cce1587dad792af5948dca56b3c
Vikalp Chakravorty <vicks548@gmail.com>
2021-06-14 12:43:01 +0530

    LeetCode solutions

---
+++ Container With Most Water.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int l=0;
+        int r=height.size()-1;
+        int maxWater=0;
+        while(l < r){
+            int lh = height[l];
+            int rh = height[r];
+            maxWater = max(maxWater, (r-l)*min(lh,rh));
+            if(lh < rh) {
+                l++;
+            } else {
+                r--;
+            }
+        }
+        return maxWater;
+    }
+};
