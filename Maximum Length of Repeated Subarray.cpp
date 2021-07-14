11ed84f161734ea52383d7fcee6403573bbfe187
Vikalp Chakravorty <vicks548@gmail.com>
2021-07-14 22:54:45 +0530

    LeetCode solutions

---
+++ Maximum Length of Repeated Subarray.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int findLength(vector<int>& nums1, vector<int>& nums2) {
+        int m = nums1.size();
+        int n = nums2.size();
+        int ans = 0;
+        vector<vector<int>>dp(m+1, vector<int>(n+1));
+        for(int i=1; i<dp.size(); i++) {
+            for(int j=1; j<dp[0].size(); j++) {
+                if(nums1[i-1] == nums2[j-1]) {
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                } else {
+                    dp[i][j] = 0;
+                }
+                ans = max(ans, dp[i][j]);
+            }
+        }
+        return ans;
+    }
+};
