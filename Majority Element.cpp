ac24db154483e8aad64ef31afd3427ee2d10d318
Vikalp Chakravorty <vicks548@gmail.com>
2021-07-19 22:01:03 +0530

    LeetCode solutions

---
+++ Majority Element.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int element = 0;
+        int count = 0;
+        for(int i=0; i<nums.size(); i++) {
+            if(count == 0) {
+                element = nums[i];
+            }
+            if(nums[i] == element) {
+                count++;
+            }
+            else {
+                count--;
+            }
+        }
+        return element;
+    }
+};
