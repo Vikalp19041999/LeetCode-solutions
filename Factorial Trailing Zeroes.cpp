d302150a6a2e154cf998a419e66c56bcf743583f
Vikalp Chakravorty <vicks548@gmail.com>
2021-08-18 21:43:21 +0530

    LeetCode solutions

---
+++ Factorial Trailing Zeroes.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int trailingZeroes(int n) {
+        int count=0;
+        while(n/5 >= 1) {
+          count = count + (n/5);
+          n = n/5;  
+        }    
+        return count;
+    }
+};
