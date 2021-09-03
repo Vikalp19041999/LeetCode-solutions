73207a44ba1dc362743c0550d34be9dc519feafc
Vikalp Chakravorty <vicks548@gmail.com>
2021-09-03 22:10:48 +0530

    LeetCode solutions

---
+++ Design Add and Search Words Data Structure.cpp
@@ -0,0 +1,72 @@
+class WordDictionary {
+public:
+    /** Initialize your data structure here. */
+    struct node {
+        char c;
+        int end;
+        node *child[26];
+    };
+    
+    node *getNode(char c) {
+        node *newnode=new node;
+        newnode->c = c;
+        newnode->end = 0;
+        for(int i=0; i<26; i++) {
+            newnode->child[i] = NULL;
+        }
+        return newnode;
+    }
+    node *root = getNode('/');
+    WordDictionary() {
+        
+    }
+    
+    bool Trie_search(string s, node *curr, int pos, int n) {        
+        if(s[pos] == '.') {
+            bool res = false;
+            node *current = curr;
+            for(int i=0; i<26; ++i) {
+                if(pos==n-1 && curr->child[i]) {
+                    current=curr->child[i];
+                    res |= current->end>0?true:false;
+                } else if(curr->child[i] && Trie_search(s,curr->child[i],pos+1,n)) {
+                    return true;
+                }
+            }
+            return res;
+        } else if(curr->child[s[pos]-'a']) {
+            if(pos == n-1) {
+                curr = curr->child[s[pos]-'a'];
+                return curr->end>0?true:false;
+            }
+            return Trie_search(s,curr->child[s[pos]-'a'],pos+1,n);
+        }
+        return false;
+    }
+    
+    void addWord(string word) {
+        int index;
+        int i = 0;
+        node *curr = root;
+        while(word[i]) {
+            index = word[i]-'a';
+            if(!curr->child[index]) {
+                curr->child[index] = getNode(word[i]);
+            }
+            curr = curr->child[index];
+            i++;
+        }
+        curr->end +=1;
+    }
+    
+    bool search(string word) {
+        return Trie_search(word, root, 0, word.size());
+    }
+};
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary* obj = new WordDictionary();
+ * obj->addWord(word);
+ * bool param_2 = obj->search(word);
+ */
